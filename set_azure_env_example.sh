cdd8251da0fcd9f199259b17e885ef47ae66c54c
Lingchen Zhu <zhulingchen@gmail.com>
2020-12-28 22:24:20 -0500

    add some bash scripts to set environment variables for Terraform

---
+++ set_azure_env_example.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# run this script with source or .
+# ARM_CLIENT_SECRET (service principal password) cannot be retrieved after creation but can be reset, see https://stackoverflow.com/a/62971780/4458566
+
+echo "Setting environment variables for Terraform"
+export ARM_SUBSCRIPTION_ID="00000000-0000-0000-0000-000000000000"
+export ARM_CLIENT_ID="00000000-0000-0000-0000-000000000000"
+export ARM_CLIENT_SECRET="XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+export ARM_TENANT_ID="00000000-0000-0000-0000-000000000000"
+export ARM_ACCESS_KEY="****************************************************************************************"  # see https://docs.microsoft.com/en-us/azure/developer/terraform/store-state-in-azure-storage
+echo "Done"
