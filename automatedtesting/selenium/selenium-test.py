806d1d5aad9ffa5140d8c40e52ddf63f24abce20
Lingchen Zhu <zhulingchen@gmail.com>
2020-12-30 23:46:49 -0500

    use the -o option of unzip to overwrite files

--- automatedtesting/selenium/selenium-test.py
+++ automatedtesting/selenium/selenium-test.py
@@ -8,6 +8,7 @@ def login (user, password):
     print ('Starting the browser...')
     # --uncomment when running in Azure DevOps.
     options = ChromeOptions()
+    options.add_argument('--no-sandbox')
     options.add_argument("--headless") 
     driver = webdriver.Chrome(options=options)
     driver = webdriver.Chrome()
