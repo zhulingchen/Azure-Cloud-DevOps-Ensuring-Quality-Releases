4f4096916264a2a0f2b7680143a2a9d6284e5044
Lingchen Zhu <zhulingchen@gmail.com>
2020-12-31 15:46:58 -0500

    test add cart and remove cart operations

--- automatedtesting/selenium/selenium-test.py
+++ automatedtesting/selenium/selenium-test.py
@@ -19,7 +19,32 @@ def login(user, password):
     product_label = driver.find_element_by_css_selector("div[class='product_label']").text
     assert "Products" in product_label
     print('Login with username {:s} and password {:s} successfully.'.format(user, password))
+    return driver
 
-login('standard_user', 'secret_sauce')
+def add_cart(driver, n_items):
+    for i in range(n_items):
+        element = "a[id='item_" + str(i) + "_title_link']"  # Get the URL of the product
+        driver.find_element_by_css_selector(element).click()  # Click the URL
+        driver.find_element_by_css_selector("button.btn_primary.btn_inventory").click()  # Add the product to the cart
+        product = driver.find_element_by_css_selector("div[class='inventory_details_name']").text  # Get the name of the product from the page
+        print(product + " added to shopping cart.")  # Display message saying which product was added
+        driver.find_element_by_css_selector("button.inventory_details_back_button").click()  # Click the Back button
+    print('{:d} items are all added to shopping cart successfully.'.format(n_items))
 
-print('Selenium tests are all successfully completed!')
+def remove_cart(driver, n_items):
+    for i in range(n_items):
+        element = "a[id='item_" + str(i) + "_title_link']"
+        driver.find_element_by_css_selector(element).click()
+        driver.find_element_by_css_selector("button.btn_secondary.btn_inventory").click()
+        product = driver.find_element_by_css_selector("div[class='inventory_details_name']").text
+        print(product + " removed from shopping cart.")  # Display message saying which product was added
+        driver.find_element_by_css_selector("button.inventory_details_back_button").click()
+    print('{:d} items are all removed from shopping cart successfully.'.format(n_items))
+
+
+if __name__ == "__main__":
+    N_ITEMS = 6
+    driver = login('standard_user', 'secret_sauce')
+    add_cart(driver, N_ITEMS)
+    remove_cart(driver, N_ITEMS)
+    print('Selenium tests are all successfully completed!')
