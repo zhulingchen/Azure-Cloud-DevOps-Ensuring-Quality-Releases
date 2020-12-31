9b9f6ea9046e576457a830b4ae5a08b96ef52e23
Lingchen Zhu <zhulingchen@gmail.com>
2020-12-30 23:00:09 -0500

    deploy a Ubuntu 18.04 VM instead of a Ubuntu 16.04 one

--- terraform/modules/vm/vm.tf
+++ terraform/modules/vm/vm.tf
@@ -29,7 +29,7 @@ resource "azurerm_linux_virtual_machine" "test" {
   source_image_reference {
     publisher = "Canonical"
     offer     = "UbuntuServer"
-    sku       = "16.04-LTS"
+    sku       = "18.04-LTS"
     version   = "latest"
   }
 }
