c9eacb217cb6a93e628c7fe51e58c1c6e69301f6
Lingchen Zhu <zhulingchen@gmail.com>
2020-12-31 15:21:38 -0500

    update login function

--- automatedtesting/selenium/selenium-test.py
+++ automatedtesting/selenium/selenium-test.py
@@ -4,16 +4,22 @@ from selenium.webdriver.chrome.options import Options as ChromeOptions
 
 
 # Start the browser and login with standard_user
-def login (user, password):
+def login(user, password):
     print ('Starting the browser...')
-    # --uncomment when running in Azure DevOps.
     options = ChromeOptions()
     options.add_argument('--no-sandbox')
     options.add_argument("--headless") 
     driver = webdriver.Chrome(options=options)
-    print ('Browser started successfully. Navigating to the demo page to login.')
+    print('Browser started successfully. Navigating to the demo page to login.')
     driver.get('https://www.saucedemo.com/')
+    # login
+    driver.find_element_by_css_selector("input[id='user-name']").send_keys(user)
+    driver.find_element_by_css_selector("input[id='password']").send_keys(password)
+    driver.find_element_by_id("login-button").click()
+    product_label = driver.find_element_by_css_selector("div[class='product_label']").text
+    assert "Products" in product_label
+    print('Login with username {:s} and password {:s} successfully.'.format(user, password))
 
 login('standard_user', 'secret_sauce')
 
-print('Selenium Test Finished!')
+print('Selenium tests are all successfully completed!')
