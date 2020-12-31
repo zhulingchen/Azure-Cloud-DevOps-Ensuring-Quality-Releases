669c4eb9042440c6ee2d151e9c5fc6ed760745dc
Lingchen Zhu <zhulingchen@gmail.com>
2020-12-30 23:56:10 -0500

    verify versions of chromium-browser and chromedriver

--- automatedtesting/selenium/selenium-test.py
+++ automatedtesting/selenium/selenium-test.py
@@ -7,11 +7,10 @@ from selenium.webdriver.chrome.options import Options as ChromeOptions
 def login (user, password):
     print ('Starting the browser...')
     # --uncomment when running in Azure DevOps.
-    options = ChromeOptions('/usr/bin/chromedriver')
+    options = ChromeOptions()
     options.add_argument('--no-sandbox')
     options.add_argument("--headless") 
     driver = webdriver.Chrome(options=options)
-    driver = webdriver.Chrome()
     print ('Browser started successfully. Navigating to the demo page to login.')
     driver.get('https://www.saucedemo.com/')
 
